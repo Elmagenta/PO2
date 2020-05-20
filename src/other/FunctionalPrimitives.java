2acea935fd2910e11145612f87ae3fd316d49630
Elmagenta <34676469+Elmagenta@users.noreply.github.com>
2020-05-20 14:37:50 +0200

    Lezione 17
    Ha parlato per 3/4 di lezione di compilazione.
    In ultimo ha fatto vedere quanto sia difficile fare la fold ricorsiva.

--- src/other/FunctionalPrimitives.java
+++ src/other/FunctionalPrimitives.java
@@ -6,6 +6,7 @@ import zoo.Dog;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -15,7 +16,7 @@ public class FunctionalPrimitives {
     public static <A, B> List<B> map(Iterable<A> l, Function<? super A, ? extends B> f) {
         List<B> c = new ArrayList<>();
 
-        for (A a: l) {
+        for (A a : l) {
             B b = f.apply(a);
             c.add(b);
         }
@@ -23,14 +24,29 @@ public class FunctionalPrimitives {
         return c;
     }
 
+    public static <A, B> List<B> map2(Iterable<A> l, Function<? super A, ? extends B> f) {
+        return fold(l, new ArrayList<>(), (x, acc) -> {
+            acc.add(f.apply(x));
+            return acc;
+        });
+    }
+
     public static <A, B> B fold(Iterable<A> l, B zero, BiFunction<A, B, B> f) {
         B acc = zero;
 
-        for (A a: l) {
+        for (A a : l) {
             acc = f.apply(a, acc);
         }
 
         return acc;
+    }
+
+    public static <A, B> B fold_recur(Iterable<A> l, B zero, BiFunction<A, B, B> f) {
+        return fold_recur(l.iterator(), zero, f);
+    }
+
+    public static <A, B> B fold_recur(Iterator<A> it, B zero, BiFunction<A, B, B> f) {
+        return it.hasNext() ? fold_recur(it, f.apply(it.next(), zero), f) : zero;
     }
 
     public static void main(String[] args) {
@@ -44,7 +60,7 @@ public class FunctionalPrimitives {
         Collection<Integer> r1 = map(l, (x) -> x * 2);
 
         /* Lista di Cani: Spiegazione dei wildcards utilizzati per dare in input alla map qualcosa di superiore a Dog e
-        * dare come tipo di ritorno qualcosa di più specifico di Dog. */
+         * dare come tipo di ritorno qualcosa di più specifico di Dog. */
         List<Dog> l2 = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
